1d19a4d79a4a1ae7878b8ef24d94c9b7f9ce9ef8
Paul Martin <greatestloginnameever@gmail.com>
2023-11-01 13:41:32 +0000

    Add icon and action log

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import base64
 import tkinter as tk
 import tkinter.messagebox as messagebox
 import copy
 from datetime import datetime
+import argparse
+import logging
+import os
 from typing import Callable, TypedDict, Literal
 
 
-undo_snapshot_size = 5
-geometry = "900x300"
-gui_state = "zoomed"
+default_undo_snapshot_size = 5
+geometry = "1500x500"
+gui_state = "normal"
 default_team1_color = "Stripes"
 default_team2_color = "Solids"
+icon_fname = "00546aecf458d72e9e2c3e9457a14a5f.ico"
+
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+icon = os.path.join(current_directory, icon_fname)
 
 
 def calculate_percentage(numerator: float, denominator: float) -> float:
@@ -20,13 +29,13 @@ class TeamStats(TypedDict):
     visits: int
     easy_shots: int
     difficult_shots: int
-    unexpected_shots: int
+    safety_shots: int
     break_shots: int
     easy_potted: int
     difficult_potted: int
-    unexpected_potted: int
+    safety_potted: int
     break_potted: int
-    additional_pots: int
+    additional_potted: int
     fouls: int
     foul_only_shots: int
 
@@ -41,8 +50,8 @@ TeamsLiteral = Literal["team1", "team2"]
 
 
 StatsLiteral = Literal[
-    'visits', 'easy_shots', 'difficult_shots', 'unexpected_shots', 'break_shots',
-    'easy_potted', 'difficult_potted', 'unexpected_potted', 'break_potted', 'additional_pots', 'fouls'
+    'visits', 'easy_shots', 'difficult_shots', 'safety_shots', 'break_shots',
+    'easy_potted', 'difficult_potted', 'safety_potted', 'break_potted', 'additional_potted', 'fouls'
 ]
 
 
@@ -56,11 +65,11 @@ class PoolStatsApp(tk.Tk):
             'easy_potted': 0,
             'difficult_shots': 0,
             'difficult_potted': 0,
-            'unexpected_shots': 0,
-            'unexpected_potted': 0,
+            'safety_shots': 0,
+            'safety_potted': 0,
             'break_shots': 0,
             'break_potted': 0,
-            'additional_pots': 0,
+            'additional_potted': 0,
             'fouls': 0,
             'foul_only_shots': 0
         }
@@ -70,8 +79,10 @@ class PoolStatsApp(tk.Tk):
         self.team_stats["team1"] = self.get_starting_team_stats()
         self.team_stats["team2"] = self.get_starting_team_stats()
 
-    def __init__(self, master=None):
+    def __init__(self, master=None, undo_snapshot_size: int = default_undo_snapshot_size):
         super().__init__(master)
+        self.iconbitmap(icon)
+        self.undo_snapshot_size = undo_snapshot_size
         self.master = master
         self.title("Pool Game Statistics")
         self.geometry(geometry)
@@ -98,22 +109,26 @@ class PoolStatsApp(tk.Tk):
         """Store the current state of stats for undo operation."""
         snapshot = copy.deepcopy(self.team_stats)
         # Check if stats_history length exceeds the limit
-        if len(self.stats_history) >= undo_snapshot_size:
+        if len(self.stats_history) >= self.undo_snapshot_size:
             self.stats_history.pop(0)  # Remove the oldest snapshot
         self.stats_history.append(snapshot)
 
     def undo(self, btn: tk.Button) -> None:
         """Undo the last recorded action."""
+        logging.debug('Undo requested')
+        self.add_action("Undo")
         self.provide_button_feedback(btn)
 
         if self.stats_history:
+            current_team_stats = self.team_stats
             self.team_stats = self.stats_history.pop()
-            self.update_stats_display()
+            self.update_stats_display(current_team_stats)
         else:
             messagebox.showwarning("Undo", "Cannot undo anymore!")
 
     def reset(self, btn: tk.Button) -> None:
         """Reset all stats."""
+        logging.debug('Reset requested')
         self.provide_button_feedback(btn)
 
         result = messagebox.askyesno("Confirmation", "Are you sure you want to reset all stats?")
@@ -130,7 +145,8 @@ class PoolStatsApp(tk.Tk):
 
             if default_team1_color not in self.team1_label.cget("text"):
                 self.toggle_teams_colors()
-            self.update_stats_display()
+            self.update_stats_display(bold_changes=False)
+            self.action_log.delete("1.0", tk.END)
 
     def create_button_frame(self, column_index: int, team: TeamsLiteral) -> tk.Frame:
         frame = tk.Frame(self, padx=10, pady=10)
@@ -141,11 +157,11 @@ class PoolStatsApp(tk.Tk):
             ('Difficult shot missed', 'difficult_shots'),
             ('Easy shot potted', 'easy_potted'),
             ('Easy shot missed', 'easy_shots'),
-            ('Safety unexpected pot', 'unexpected_potted'),
-            ('Safety shot', 'unexpected_shots'),
+            ('Safety unexpected pot', 'safety_potted'),
+            ('Safety shot', 'safety_shots'),
             ('Break shot potted', 'break_potted'),
             ('Break shot missed', 'break_shots'),
-            ('Pot Existing Shot', 'additional_pots'),
+            ('Pot Existing Shot', 'additional_potted'),
             ('Foul Existing Shot', 'fouls'),
             ('Foul Only', 'foul_only_shots')
         ]
@@ -171,6 +187,12 @@ class PoolStatsApp(tk.Tk):
         btn = tk.Button(self, text=text)
         btn.configure(command=lambda btn=btn: callback(btn))
         btn.grid(row=row, column=column, pady=20)
+
+    def add_action(self, text: str) -> None:
+        current_time = datetime.now().strftime('%H:%M:%S')
+        message = f'{current_time}: {text}\n'
+        self.action_log.insert('1.0', message)
+        self.action_log.see('1.0')
 
     def setup_ui(self) -> None:
         # Team 1 Buttons
@@ -210,6 +232,19 @@ class PoolStatsApp(tk.Tk):
         self.add_action_button("Export Stats", 2, 3, self.export_stats)
         self.add_action_button("Reset", 2, 4, self.reset)
 
+        # Add the new Text widget for action logging along the bottom
+        self.action_log_label = tk.Label(self, text="Action Log", font=("Arial", 16))
+        self.action_log_label.grid(row=0, column=5, pady=5, sticky='nw')  # Span the entire width
+
+        self.action_log = tk.Text(self, wrap=tk.NONE, width=45, height=20)  # Adjust width/height as needed
+        self.action_log.grid(row=1, rowspan=3, column=5, padx=5, pady=5, sticky='nsew')  # Span the entire width
+
+        # Create a vertical scrollbar
+        self.action_log_scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.action_log.yview)
+        self.action_log_scrollbar.grid(row=1, column=6, rowspan=2, sticky='nsew')
+
+        self.action_log.config(yscrollcommand=self.action_log_scrollbar.set)
+
     def toggle_teams_colors(self, reset: bool = False):
         # Check the current label text for Team 1
         if reset or default_team1_color not in self.team1_label.cget("text"):
@@ -226,6 +261,8 @@ class PoolStatsApp(tk.Tk):
     def complete_game(self, btn: tk.Button):
         self.provide_button_feedback(btn)
         self.end_time = datetime.now()
+        logging.debug('Game completed')
+        self.add_action("Game complete")
         self.update_stats_display()
 
     def provide_button_feedback(self, btn: tk.Button):
@@ -236,6 +273,19 @@ class PoolStatsApp(tk.Tk):
 
     def record_action(self, team: TeamsLiteral, action: str, btn: tk.Button):
         self.provide_button_feedback(btn)
+
+        team_name = team.replace("team", "Team ")
+        action_name = action.replace("_", " ").replace("potted", "ball potted").replace("shots", "shot").replace('fouls', 'Foul').capitalize()
+        if action_name.endswith('shot'):
+            action_name = f'{action_name} missed'
+        print(action, action_name)
+        message = f"{team_name} {action_name}"
+
+        self.add_action(message)
+
+        logging.debug('')
+        logging.debug("%s %s", team_name, action_name)
+
         if "break" in action and self.team_stats[team]['break_shots'] > 0:
             messagebox.showerror("Error", "Only one break shot allowed!")
             return
@@ -253,7 +303,7 @@ class PoolStatsApp(tk.Tk):
             self.active_team = team
             self.team_stats[team]['visits'] += 1
 
-        if action in ['difficult_potted', 'easy_potted', 'unexpected_potted', 'break_potted']:
+        if action in ['difficult_potted', 'easy_potted', 'safety_potted', 'break_potted']:
             shot_type = action.split('_')[0]
             self.team_stats[team][f"{shot_type}_shots"] += 1
 
@@ -264,13 +314,14 @@ class PoolStatsApp(tk.Tk):
         self.update_stats_display()
 
     @staticmethod
-    def generate_stats_text(stats: TeamStats, prev_stats: TeamStats) -> list[tuple[str, str]]:
+    def generate_stats_text(stats: TeamStats, prev_stats: TeamStats, name: str = "",
+                            bold_changes: bool = True) -> list[tuple[str, str]]:
         total_pot_attempts = stats['easy_shots'] + stats['difficult_shots']
-        total_shots = stats['easy_shots'] + stats['difficult_shots'] + stats['unexpected_shots'] + stats[
+        total_shots = stats['easy_shots'] + stats['difficult_shots'] + stats['safety_shots'] + stats[
             'break_shots'] + stats['foul_only_shots']
         total_potted = stats['easy_potted'] + stats['difficult_potted']
         total_potted_with_break = total_potted + stats['break_potted']
-        total_pots_with_additional = total_potted_with_break + stats['unexpected_potted'] + stats['additional_pots']
+        total_pots_with_additional = total_potted_with_break + stats['safety_potted'] + stats['additional_potted']
 
         shot_percent = calculate_percentage(total_pots_with_additional, total_shots)
         pot_percent = calculate_percentage(total_potted, total_pot_attempts)
@@ -281,9 +332,15 @@ class PoolStatsApp(tk.Tk):
         text = []
         k: StatsLiteral
         for k, v in stats.items():
+            key_name = k.replace('_', ' ').capitalize()
+            if (prev_value := prev_stats[k]) == v:
+                tag = ""
+            else:
+                if name:
+                    logging.debug("%s %s changed from %s to %s", name, key_name, prev_value, v)
+                tag = "bold" if bold_changes else ""
             if not k == "foul_only_shots":
-                tag = "" if v == prev_stats[k] else "bold"
-                text.append((tag, f"{k.replace('_', ' ').capitalize()}: {v}"))
+                text.append((tag, f"{key_name}: {v}"))
 
         text.append(("", f"Total shots: {total_shots}"))
         text.append(("", f"Pot %: {pot_percent:.2f}"))
@@ -295,23 +352,24 @@ class PoolStatsApp(tk.Tk):
 
         return text
 
-    def update_stats_display(self) -> None:
-        if self.stats_history:
-            prev_stats = self.stats_history[-1]
-        else:
-            prev_stats = self.team_stats
+    def update_stats_display(self, prev_stats: OverallStats = None, bold_changes: bool = True) -> None:
+        if not prev_stats:
+            prev_stats = self.stats_history[-1] if self.stats_history else self.team_stats
 
-        team1_stats_text = self.generate_stats_text(self.team_stats['team1'], prev_stats['team1'])
+        team1_stats_text = self.generate_stats_text(self.team_stats['team1'], prev_stats['team1'], "Team 1",
+                                                    bold_changes=bold_changes)
         self.update_text_widget(self.team1_stats_text, team1_stats_text)
 
-        team2_stats_text = self.generate_stats_text(self.team_stats['team2'], prev_stats['team2'])
+        team2_stats_text = self.generate_stats_text(self.team_stats['team2'], prev_stats['team2'], "Team 2",
+                                                    bold_changes=bold_changes)
         self.update_text_widget(self.team2_stats_text, team2_stats_text)
 
         k: StatsLiteral
         for k in self.team_stats['team1']:
             self.team_stats['total'][k] = self.team_stats['team1'][k] + self.team_stats['team2'][k]
 
-        total_stats_text = self.generate_stats_text(self.team_stats['total'], prev_stats['total'])
+        total_stats_text = self.generate_stats_text(self.team_stats['total'], prev_stats['total'],
+                                                    bold_changes=bold_changes)
         self.update_text_widget(self.total_stats_text, total_stats_text)
 
     def update_text_widget(self, widget: tk.Text, text: list[tuple[str, str]]):
@@ -331,7 +389,7 @@ class PoolStatsApp(tk.Tk):
         # Extracting the stats in order and converting to a tab-delimited string
         stats_order = [
             'visits', 'easy_shots', 'easy_potted', 'difficult_shots', 'difficult_potted',
-            'unexpected_shots', 'unexpected_potted', 'break_shots', 'break_potted', 'additional_pots',
+            'safety_shots', 'safety_potted', 'break_shots', 'break_potted', 'additional_potted',
             'fouls', 'foul_only_shots'
         ]
 
@@ -354,9 +412,24 @@ class PoolStatsApp(tk.Tk):
         self.clipboard_clear()
         self.clipboard_append(export_string)
         self.update()  # This is to make sure the clipboard is updated immediately
-        messagebox.showinfo("Exported", "Stats have been copied to clipboard!")
+        messagebox.showinfo("Exported",
+                            "Team 1 and Team 2 Stats have been copied to clipboard for pasting into a spreadsheet!")
 
 
 if __name__ == "__main__":
-    app = PoolStatsApp()
+    parser = argparse.ArgumentParser(description="A tool for tracking pool stats")
+
+    parser.add_argument("-u", "--undo-snapshot-size", type=int, default=default_undo_snapshot_size,
+                        help="Size of the undo snapshot (integer value).")
+
+    parser.add_argument("--log-level", type=str, choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        default='DEBUG', help="Set the logging level.")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    app = PoolStatsApp(undo_snapshot_size=args.undo_snapshot_size)
     app.mainloop()
